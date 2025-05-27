5e4d6f8617cbfccdfa1360e306a55a37aac99707
thanhlap <116256175+lythanhlap@users.noreply.github.com>
2025-05-27 22:56:57 +0700

    Add files via upload

---
+++ ngrok1_git_repo/install_bot.sh
@@ -0,0 +1,15 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+echo "[+] Đang cài gói cần thiết..."
+pkg update -y
+pkg install -y wget unzip curl jq python
+
+echo "[+] Tải bot về..."
+cd $HOME
+mkdir -p .telebot && cd .telebot
+
+wget https://github.com/lythanhlap/ngrok1/blob/7e5ede31a58dd4279b1401dcfb60af04d687ff80/ngrok1_git_repo/telebot.sh -O telebot.sh
+chmod +x telebot.sh
+
+echo "[+] Chạy bot Telegram..."
+./telebot.sh
