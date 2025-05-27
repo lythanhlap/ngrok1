24a32b2ffc30ca4350968823ddd4f44b400003f2
thanhlap <116256175+lythanhlap@users.noreply.github.com>
2025-05-27 23:33:08 +0700

    Update install_bot.sh

--- ngrok1_git_repo/install_bot.sh
+++ ngrok1_git_repo/install_bot.sh
@@ -8,7 +8,7 @@ echo "[+] Tải bot về..."
 cd $HOME
 mkdir -p .telebot && cd .telebot
 
-wget https://raw.githubusercontent.com/lythanhlap/ngrok1/blob/7e5ede31a58dd4279b1401dcfb60af04d687ff80/ngrok1_git_repo/telebot.sh -O telebot.sh
+wget https://raw.githubusercontent.com/lythanhlap/ngrok1/7e5ede31a58dd4279b1401dcfb60af04d687ff80/ngrok1_git_repo/telebot.sh -O telebot.sh
 chmod +x telebot.sh
 
 echo "[+] Chạy bot Telegram..."
